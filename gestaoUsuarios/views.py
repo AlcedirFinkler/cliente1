92fd57ea96a8bf655d231b05456b021443cd64cb
belaus-m <miguelbelaus0@gmail.com>
2025-04-27 14:09:57 -0300

    responsividade mobile para manutencaoPrev
    ajustado a responsividade em mobiles para as tabelas de manutenção preventiva.
    adicionado um sistema de "cards" para cada manutenção individual.
    omitido o grafico gantt para apresentação

--- gestaoUsuarios/views.py
+++ gestaoUsuarios/views.py
@@ -205,8 +205,7 @@ def get_user_modules(user):
         {"name": "Mant. Preventivas", "url": reverse('listar_manutencoes'), "icon": "bi-tools", "enabled": False,
           "submenu": [
              {"name": "Tabelas", "url": reverse('listar_manutencoes')},
-             {"name": "Calendário", "url": reverse('calendario_view')},
-             {"name": "Gráfico Gantt", "url": reverse('gantt_manutencoes')}
+             {"name": "Calendário", "url": reverse('calendario_view')}
          ]},
         {"name": "Controle Estoque", "url": reverse('cadEstoque'), "icon": "bi-box", "enabled": False},
         {"name": "Cadastro Equipamentos", "url": reverse('CadEquip'), "icon": "bi-gear", "enabled": False},
